241a6c4536c737ac1b204d33d2c2ac4c8718958d
Elliot Lefkovitz <e.lefkovitz@gmail.com>
2025-04-04 16:45:33 -0400

    Small error edit on handlers

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -4,7 +4,7 @@ from app import db
 from app.errors import bp
 from app.api.errors import error_response as api_error_response
 
-def wants_json_response:
+def wants_json_response():
     return request.accept_mimetypes['application/json'] >= \
         request.accept_mimetypes['text.html']
 
